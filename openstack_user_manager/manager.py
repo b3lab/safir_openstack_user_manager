3afa97fcdc2c80fc0f824e1b0d9b225ba404c8c5
celik.esra <celik.esra@tubitak.gov.tr>
2017-12-12 11:36:14 +0300

    Safir Billing customer generation added

--- openstack_user_manager/manager.py
+++ openstack_user_manager/manager.py
@@ -16,10 +16,19 @@ from keystoneclient.v3 import client as keystone_client
 from neutronclient.v2_0 import client as neutron_client
 from neutronclient.common import exceptions as n_exceptions
 from openstack import connection
+from openstack import exceptions as o_exceptions
 from openstack_user_manager import log
 from os_client_config import config as cloud_config
 
+try:
+    from safirbillingclient import client as safirbilling_client
+    from safirbillingclient import exc as sb_exceptions
+except:
+    pass
+
 LOG = log.get_logger()
+
+INITIAL_CREDIT_AMOUNT=250
 
 
 class Opts(object):
@@ -60,6 +69,19 @@ class OpenstackUserManager:
         self.keystone_conn = keystone_client.Client(
             session=cloud.get_session_client('identity'))
 
+        try:
+            self.safirbilling_conn = safirbilling_client.Client(
+                '1',
+                cloud.get_endpoint('billing'),
+                session=cloud.get_session_client('billing'))
+        except o_exceptions.SDKException as ex:
+            LOG.error('Could connect to billing service. ' +
+                      ex.message)
+            self.safirbilling_conn = None
+        except Exception:
+            LOG.error('Could connect to billing service.')
+            self.safirbilling_conn = None
+
     def check_username_availability(self,
                                     user_name):
         try:
@@ -93,6 +115,7 @@ class OpenstackUserManager:
         except ka_exceptions.ClientException as ex:
             LOG.error("Project not created. Error: " + ex.message)
             return False
+
         return True
 
     def create_user(self, email, user_name, password, enabled=False):
@@ -103,6 +126,52 @@ class OpenstackUserManager:
                                            enabled=enabled)
         except ka_exceptions.ClientException as ex:
             LOG.error("User not created. Error: " + ex.message)
+            return False
+        return True
+
+    def init_billing_customer(self,
+                              project_name,
+                              customer_name,
+                              company_name,
+                              email):
+        if self.safirbilling_conn is None:
+            return False
+
+        try:
+            project = self.conn.identity.find_project(project_name)
+
+            # Add customer
+            self.safirbilling_conn.customer.set(project_id=project.id,
+                                                contact_name=customer_name,
+                                                company_name=company_name,
+                                                mail=email)
+
+            # Load initial credit
+            self.safirbilling_conn.credit.load(project_id=project.id,
+                                               amount=INITIAL_CREDIT_AMOUNT)
+
+            # Add rating role
+            user = self.conn.identity.find_user('cloudkitty')
+            if user is not None:
+                role = self.conn.identity.find_role('rating')
+                if role is not None:
+                    self.keystone_conn.roles.grant(role,
+                                                   user=user,
+                                                   project=project)
+
+            # Add billing role
+            user = self.conn.identity.find_user('safirbilling')
+            if user is not None:
+                role = self.conn.identity.find_role('billing')
+                if role is not None:
+                    self.keystone_conn.roles.grant(role,
+                                                   user=user,
+                                                   project=project)
+        except sb_exceptions.HTTPException as ex:
+            LOG.error("Billing customer not created. Error: " + ex.message)
+            return False
+        except Exception as ex:
+            LOG.error("Billing customer not created. Error: " + ex.message)
             return False
         return True
 
