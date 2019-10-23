00710e9ec2ef48a4819a9e0c799075c4adfd14a5
rootz1one <root.mch@gmail.com>
2019-10-23 16:03:27 +0300

    OpenStack User Manager updated for OpenStack Stein.

--- openstack_user_manager/manager.py
+++ openstack_user_manager/manager.py
@@ -22,11 +22,11 @@ from os_client_config import config as cloud_config
 
 import datetime
 
-try:
-    from safirbillingclient import client as safirbilling_client
-    from safirbillingclient import exc as sb_exceptions
-except:
-    pass
+#try:
+#    from safirbillingclient import client as safirbilling_client
+#    from safirbillingclient import exc as sb_exceptions
+#except:
+#    pass
 
 LOG = log.get_logger()
 
@@ -71,18 +71,18 @@ class OpenstackUserManager:
         self.keystone_conn = keystone_client.Client(
             session=cloud.get_session_client('identity'))
 
-        try:
-            self.safirbilling_conn = safirbilling_client.Client(
-                '1',
-                cloud.get_endpoint('billing'),
-                session=cloud.get_session_client('billing'))
-        except o_exceptions.SDKException as ex:
-            LOG.error('Could connect to billing service. ' +
-                      ex.message)
-            self.safirbilling_conn = None
-        except Exception:
-            LOG.error('Could connect to billing service.')
-            self.safirbilling_conn = None
+#        try:
+#            self.safirbilling_conn = safirbilling_client.Client(
+#                '1',
+#                cloud.get_endpoint('billing'),
+#                session=cloud.get_session_client('billing'))
+#        except o_exceptions.SDKException as ex:
+#            LOG.error('Could connect to billing service. ' +
+#                      ex.message)
+#            self.safirbilling_conn = None
+#        except Exception:
+#            LOG.error('Could connect to billing service.')
+#            self.safirbilling_conn = None
 
     def check_username_availability(self,
                                     user_name):
@@ -131,103 +131,103 @@ class OpenstackUserManager:
             return False
         return True
 
-    def init_billing_customer(self,
-                              project_name,
-                              customer_name,
-                              company_name,
-                              email):
-        if self.safirbilling_conn is None:
-            return False
-
-        try:
-            project = self.conn.identity.find_project(project_name)
-        except sb_exceptions.HTTPException as ex:
-            LOG.error("Billing customer not created. Error: " + ex.message)
-            return False
-        except Exception as ex:
-            LOG.error("Billing customer not created. Error: " + ex.message)
-            return False
-
-        try:
-            # Add customer
-            self.safirbilling_conn.customer.set(project_id=project.id,
-                                                contact_name=customer_name,
-                                                company_name=company_name,
-                                                mail=email)
-        except sb_exceptions.HTTPException as ex:
-            pass
-        except Exception as ex:
-            pass
-
-        try:
-            # Load initial credit
-            self.safirbilling_conn.credit.load(project_id=project.id,
-                                               amount=INITIAL_CREDIT_AMOUNT)
-        except sb_exceptions.HTTPException as ex:
-            pass
-        except Exception as ex:
-            pass
-
-        try:
-            # Add rating role
-            user = self.conn.identity.find_user('cloudkitty')
-            if user is not None:
-                role = self.conn.identity.find_role('rating')
-                if role is not None:
-                    self.keystone_conn.roles.grant(role,
-                                                   user=user,
-                                                   project=project)
-
-            # Add billing role
-            user = self.conn.identity.find_user('safirbilling')
-            if user is not None:
-                role = self.conn.identity.find_role('billing')
-                if role is not None:
-                    self.keystone_conn.roles.grant(role,
-                                                   user=user,
-                                                   project=project)
-        except sb_exceptions.HTTPException as ex:
-            LOG.error("Billing customer not created. Error: " + ex.message)
-            return False
-        except Exception as ex:
-            LOG.error("Billing customer not created. Error: " + ex.message)
-            return False
-        return True
-
-    def get_billing_customer_status(self, project_id):
-        if self.safirbilling_conn is None:
-            return None
-
-        try:
-            customer = self.safirbilling_conn.customer.get(project_id=project_id)
-        except sb_exceptions.HTTPException as ex:
-            LOG.error("Billing customer not found. Error: " + ex.message)
-            return False
-        except Exception as ex:
-            LOG.error("Billing customer not found. Error: " + ex.message)
-            return False
-        return customer.status
-
-    def get_customer_termination_date(self, project_id):
-        if self.safirbilling_conn is None:
-            return None
-
-        try:
-            customer = self.safirbilling_conn.customer.get(project_id=project_id)
-            termination_time_limit = self.safirbilling_conn.config.get_termination_time_limit()
-            time_delta=int(termination_time_limit[0].value)
-            suspend_time = datetime.datetime.strptime(customer.suspend_time,
-                                                      "%Y-%m-%dT%H:%M:%S")
-            termination_date = (suspend_time +
-                                datetime.timedelta(days=time_delta))
-        except sb_exceptions.HTTPException as ex:
-            LOG.error("Billing customer not found. Error: " + ex.message)
-            return None
-        except Exception as ex:
-            LOG.error("Billing customer not found. Error: " + ex.message)
-            return None
-        return termination_date
-
+#    def init_billing_customer(self,
+#                              project_name,
+#                              customer_name,
+#                              company_name,
+#                              email):
+#        if self.safirbilling_conn is None:
+#            return False
+#
+#        try:
+#            project = self.conn.identity.find_project(project_name)
+#        except sb_exceptions.HTTPException as ex:
+#            LOG.error("Billing customer not created. Error: " + ex.message)
+#            return False
+#        except Exception as ex:
+#            LOG.error("Billing customer not created. Error: " + ex.message)
+#            return False
+#
+#        try:
+#            # Add customer
+#            self.safirbilling_conn.customer.set(project_id=project.id,
+#                                                contact_name=customer_name,
+#                                                company_name=company_name,
+#                                                mail=email)
+#        except sb_exceptions.HTTPException as ex:
+#            pass
+#        except Exception as ex:
+#            pass
+#
+#        try:
+#            # Load initial credit
+#            self.safirbilling_conn.credit.load(project_id=project.id,
+#                                               amount=INITIAL_CREDIT_AMOUNT)
+#        except sb_exceptions.HTTPException as ex:
+#            pass
+#        except Exception as ex:
+#            pass
+#
+#        try:
+#            # Add rating role
+#            user = self.conn.identity.find_user('cloudkitty')
+#            if user is not None:
+#                role = self.conn.identity.find_role('rating')
+#                if role is not None:
+#                    self.keystone_conn.roles.grant(role,
+#                                                   user=user,
+#                                                   project=project)
+#
+#            # Add billing role
+#            user = self.conn.identity.find_user('safirbilling')
+#            if user is not None:
+#                role = self.conn.identity.find_role('billing')
+#                if role is not None:
+#                    self.keystone_conn.roles.grant(role,
+#                                                   user=user,
+#                                                   project=project)
+#        except sb_exceptions.HTTPException as ex:
+#            LOG.error("Billing customer not created. Error: " + ex.message)
+#            return False
+#        except Exception as ex:
+#            LOG.error("Billing customer not created. Error: " + ex.message)
+#            return False
+#        return True
+#
+#    def get_billing_customer_status(self, project_id):
+#        if self.safirbilling_conn is None:
+#            return None
+#
+#        try:
+#            customer = self.safirbilling_conn.customer.get(project_id=project_id)
+#        except sb_exceptions.HTTPException as ex:
+#            LOG.error("Billing customer not found. Error: " + ex.message)
+#            return False
+#        except Exception as ex:
+#            LOG.error("Billing customer not found. Error: " + ex.message)
+#            return False
+#        return customer.status
+#
+#    def get_customer_termination_date(self, project_id):
+#        if self.safirbilling_conn is None:
+#            return None
+#
+#        try:
+#            customer = self.safirbilling_conn.customer.get(project_id=project_id)
+#            termination_time_limit = self.safirbilling_conn.config.get_termination_time_limit()
+#            time_delta=int(termination_time_limit[0].value)
+#            suspend_time = datetime.datetime.strptime(customer.suspend_time,
+#                                                      "%Y-%m-%dT%H:%M:%S")
+#            termination_date = (suspend_time +
+#                                datetime.timedelta(days=time_delta))
+#        except sb_exceptions.HTTPException as ex:
+#            LOG.error("Billing customer not found. Error: " + ex.message)
+#            return None
+#        except Exception as ex:
+#            LOG.error("Billing customer not found. Error: " + ex.message)
+#            return None
+#        return termination_date
+#
     def get_project(self, project_name):
         try:
             project = self.conn.identity.find_project(project_name)
@@ -332,28 +332,25 @@ class OpenstackUserManager:
                 ip_version=4,
                 cidr=subnet_cidr,
                 dns_nameservers=dns_nameservers)
-
-            # CREATE ROUTER
-            # router = self.conn.network.create_router(
-            #     name=router_name,
-            #     tenant_id=project.id,
-            #     admin_state_up=True)
-            ext_net_id = [e for e in self.neutron_conn.list_networks(
-                          )['networks'] if
-                          e['name'] == external_network_name][0]['id']
-            router_param = {
-                'name': router_name,
-                'admin_state_up': True,
-                'external_gateway_info': {"network_id": ext_net_id},
-                'tenant_id': project.id}
-            router = self.neutron_conn.create_router(
-                {'router': router_param})
-
-            self.neutron_conn.add_interface_router(
-                router['router']['id'],
-                {'subnet_id': subnet.id,
-                 'tenant_id': project.id})
-
+            # Find user network by ext-network name
+            user_network = self.conn.network.find_network(external_network_name)
+            # Find ext-network-id using by user_network object
+            ext_net_id = user_network['id']
+            
+            # Set ext-gateway-info parameter, this is need for create router func
+            external_gateway_info = {"network_id": ext_net_id}
+            
+            # Create router using by openstacksdk, not neutron client
+            router = self.conn.network.create_router(
+                name=router_name,
+                project_id=project.id,
+                external_gateway_info=external_gateway_info,
+                admin_state_up=True)
+            
+            # Add private network interface to router
+            router = router.add_interface(
+                   self.conn.network, subnet_id=subnet.id)
+            
         except n_exceptions.NeutronException as ex:
             LOG.error("Project's initial network could not be defined. "
                       "Error: " + str(ex.message))
